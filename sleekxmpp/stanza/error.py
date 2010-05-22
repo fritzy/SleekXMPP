828cba875fa5466493e3ea3f804bb349743a0615
Lance Stout <lancestout@gmail.com>
2010-05-22 22:39:35 +0800

    Added the error attribute 'code' to the Error object interface.

--- sleekxmpp/stanza/error.py
+++ sleekxmpp/stanza/error.py
@@ -12,7 +12,7 @@ class Error(ElementBase):
 	name = 'error'
 	plugin_attrib = 'error'
 	conditions = set(('bad-request', 'conflict', 'feature-not-implemented', 'forbidden', 'gone', 'item-not-found', 'jid-malformed', 'not-acceptable', 'not-allowed', 'not-authorized', 'payment-required', 'recipient-unavailable', 'redirect', 'registration-required', 'remote-server-not-found', 'remote-server-timeout', 'service-unavailable', 'subscription-required', 'undefined-condition', 'unexpected-request'))
-	interfaces = set(('condition', 'text', 'type'))
+	interfaces = set(('code', 'condition', 'text', 'type'))
 	types = set(('cancel', 'continue', 'modify', 'auth', 'wait'))
 	sub_interfaces = set(('text',))
 	condition_ns = 'urn:ietf:params:xml:ns:xmpp-stanzas'

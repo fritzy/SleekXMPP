a38735cb2adabb75f110669d5425bf389cfc7ab7
Nathan Fritz <nathan@andyet.net>
2010-06-02 15:54:44 -0700

    added very, very, very basic atom stanza

---
+++ sleekxmpp/stanza/atom.py
@@ -0,0 +1,11 @@
+from .. xmlstream.stanzabase import ElementBase, ET, JID
+from xml.etree import cElementTree as ET
+
+class AtomEntry(ElementBase):
+	namespace = 'http://www.w3.org/2005/Atom'
+	name = 'entry'
+	plugin_attrib = 'entry'
+	interfaces = set(('title', 'summary'))
+	sub_interfaces = set(('title', 'summary'))
+	plugin_attrib_map = {}
+	plugin_tag_map = {}

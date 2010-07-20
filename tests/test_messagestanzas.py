f505e229d6af009ba926292e3947346614df89f9
Lance Stout <lancestout@gmail.com>
2010-07-20 01:55:44 -0400

    Updated message stanza tests.

--- tests/test_messagestanzas.py
+++ tests/test_messagestanzas.py
@@ -1,18 +1,15 @@
-import unittest
-from xml.etree import cElementTree as ET
+from sleektest import *
+from sleekxmpp.stanza.message import Message
+from sleekxmpp.stanza.htmlim import HTMLIM
 
-class testmessagestanzas(unittest.TestCase):
+class TestMessageStanzas(SleekTest):
 
 	def setUp(self):
-		import sleekxmpp.stanza.message as m
-		from sleekxmpp.basexmpp import registerStanzaPlugin
-		from sleekxmpp.stanza.htmlim import HTMLIM
-		registerStanzaPlugin(m.Message, HTMLIM)
-		self.m = m
-	
+		registerStanzaPlugin(Message, HTMLIM)
+		
 	def testGroupchatReplyRegression(self):
 		"Regression groupchat reply should be to barejid"
-		msg = self.m.Message()
+		msg = self.Message()
 		msg['to'] = 'me@myserver.tld'
 		msg['from'] = 'room@someservice.someserver.tld/somenick'
 		msg['type'] = 'groupchat'
@@ -22,23 +19,27 @@ class testmessagestanzas(unittest.TestCase):
 
 	def testAttribProperty(self):
 		"Test attrib property returning self"
-		msg = self.m.Message()
+		msg = self.Message()
 		msg.attrib.attrib.attrib['to'] = 'usr@server.tld'
 		self.failUnless(str(msg['to']) == 'usr@server.tld')
 	
 	def testHTMLPlugin(self):
 		"Test message/html/html stanza"
-		msgtxt = """<message to="fritzy@netflint.net/sleekxmpp" type="chat"><body>this is the plaintext message</body><html xmlns="http://jabber.org/protocol/xhtml-im"><body xmlns="http://www.w3.org/1999/xhtml"><p>This is the htmlim message</p></body></html></message>"""
-		msg = self.m.Message()
+		msg = self.Message()
 		msg['to'] = "fritzy@netflint.net/sleekxmpp"
 		msg['body'] = "this is the plaintext message"
 		msg['type'] = 'chat'
 		p = ET.Element('{http://www.w3.org/1999/xhtml}p')
 		p.text = "This is the htmlim message"
 		msg['html']['html'] = p
-		msg2 = self.m.Message()
-		values = msg.getValues()
-		msg2.setValues(values)
-		self.failUnless(msgtxt == str(msg) == str(msg2))
+                self.checkMessage(msg,  """
+                  <message to="fritzy@netflint.net/sleekxmpp" type="chat">
+                    <body>this is the plaintext message</body>
+                    <html xmlns="http://jabber.org/protocol/xhtml-im">
+                      <body xmlns="http://www.w3.org/1999/xhtml">
+                        <p>This is the htmlim message</p>
+                      </body>
+                    </html>
+                  </message>""")
 
-suite = unittest.TestLoader().loadTestsFromTestCase(testmessagestanzas)
+suite = unittest.TestLoader().loadTestsFromTestCase(TestMessageStanzas)

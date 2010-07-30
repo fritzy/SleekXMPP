60a183b011f8c0ee5a3c1840075a43addf33fc4f
Lance Stout <lancestout@gmail.com>
2010-07-29 20:18:04 -0400

    Added useful imports to the xmlstream, xmlstream.handler, and xmlstream.matcher __init__.py files to make it simpler to import common classes.

---
+++ sleekxmpp/xmlstream/__init__.py
@@ -0,0 +1,10 @@
+"""
+    SleekXMPP: The Sleek XMPP Library
+    Copyright (C) 2010  Nathanael C. Fritz
+    This file is part of SleekXMPP.
+
+    See the file LICENSE for copying permission.
+"""
+
+from sleekxmpp.xmlstream.stanzabase import StanzaBase, ElementBase
+from sleekxmpp.xmlstream.xmlstream import XMLStream, RESPONSE_TIMEOUT

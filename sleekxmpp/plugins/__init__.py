6d68706326e3248e167b92289fd695ed8b11cec7
Lance Stout <lancestout@gmail.com>
2010-10-25 20:37:02 -0400

    Added XEP-0012 Last Activity plugin.
    Contributed by Cesar Alcalde.

--- sleekxmpp/plugins/__init__.py
+++ sleekxmpp/plugins/__init__.py
@@ -5,5 +5,6 @@
 
     See the file LICENSE for copying permission.
 """
-__all__ = ['xep_0004', 'xep_0030', 'xep_0033', 'xep_0045', 'xep_0050',
-'xep_0078', 'xep_0085', 'xep_0092', 'xep_0199', 'gmail_notify', 'xep_0060']
+__all__ = ['xep_0004', 'xep_0012', 'xep_0030', 'xep_0033', 'xep_0045',
+           'xep_0050', 'xep_0078', 'xep_0085', 'xep_0092', 'xep_0199',
+           'gmail_notify', 'xep_0060']

1a81b2f464a3f35c99ff1659b7debb6006004d4f
Lance Stout <lancestout@gmail.com>
2011-02-24 14:15:02 -0500

    Add tests for XEP-0085, fix some bugs.

--- sleekxmpp/plugins/xep_0085/chat_states.py
+++ sleekxmpp/plugins/xep_0085/chat_states.py
@@ -14,7 +14,7 @@ from sleekxmpp.xmlstream.handler import Callback
 from sleekxmpp.xmlstream.matcher import StanzaPath
 from sleekxmpp.xmlstream import register_stanza_plugin, ElementBase, ET
 from sleekxmpp.plugins.base import base_plugin
-from sleekxmpp.plugins.xep_0085 import stanza
+from sleekxmpp.plugins.xep_0085 import stanza, ChatState
 
 
 log = logging.getLogger(__name__)
@@ -31,16 +31,17 @@ class xep_0085(base_plugin):
         self.description = 'Chat State Notifications'
         self.stanza = stanza
 
-        self.xmpp.registerHandler(
-            Callback('Chat States',
-                     StanzaPath('message/chat_state'),
-                     self._handle_chat_state))
+        for state in ChatState.states:
+            self.xmpp.register_handler(
+                Callback('Chat State: %s' % state,
+                         StanzaPath('message@chat_state=%s' % state),
+                         self._handle_chat_state))
 
-        register_stanza_plugin(Message, stanza.ChatState)
+        register_stanza_plugin(Message, ChatState)
 
     def post_init(self):
         base_plugin.post_init(self)
-        self.xmpp.plugin['xep_0030'].add_feature(stanza.ChatState.namepsace)
+        self.xmpp.plugin['xep_0030'].add_feature(ChatState.namespace)
 
     def _handle_chat_state(self, msg):
         state = msg['chat_state']

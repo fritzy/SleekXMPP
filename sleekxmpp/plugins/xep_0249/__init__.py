833f95b53af47903d8e4281e8d5668a5addc2383
Lance Stout <lancestout@gmail.com>
2011-03-23 10:00:32 -0400

    Cleaned XEP-0249 plugin, added tests.

--- sleekxmpp/plugins/xep_0249/__init__.py
+++ sleekxmpp/plugins/xep_0249/__init__.py
@@ -1,2 +1,10 @@
+"""
+    SleekXMPP: The Sleek XMPP Library
+    Copyright (C) 2011 Nathanael C. Fritz, Dalek
+    This file is part of SleekXMPP.
+
+    See the file LICENSE for copying permission.
+"""
+
 from sleekxmpp.plugins.xep_0249.stanza import Invite
 from sleekxmpp.plugins.xep_0249.invite import xep_0249
---
+++ tests/test_stream_xep_0249.py
@@ -0,0 +1,64 @@
+import sys
+import time
+import threading
+
+from sleekxmpp.test import *
+from sleekxmpp.xmlstream import ElementBase
+
+
+class TestStreamDirectInvite(SleekTest):
+
+    """
+    Test using the XEP-0249 plugin.
+    """
+
+    def tearDown(self):
+        sys.excepthook = sys.__excepthook__
+        self.stream_close()
+
+    def testReceiveInvite(self):
+        self.stream_start(mode='client',
+                          plugins=['xep_0030',
+                                   'xep_0249'])
+
+        events = []
+
+        def handle_invite(msg):
+            events.append(True)
+
+        self.xmpp.add_event_handler('groupchat_direct_invite',
+                                    handle_invite)
+
+        self.recv("""
+          <message>
+            <x xmlns="jabber:x:conference"
+               jid="sleek@conference.jabber.org"
+               password="foo"
+               reason="For testing" />
+          </message>
+        """)
+
+        time.sleep(.5)
+
+        self.failUnless(events == [True],
+                "Event not raised: %s" % events)
+
+    def testSentDirectInvite(self):
+        self.stream_start(mode='client',
+                          plugins=['xep_0030',
+                                   'xep_0249'])
+
+        self.xmpp['xep_0249'].send_invitation('user@example.com',
+                                              'sleek@conference.jabber.org',
+                                              reason='Need to test Sleek')
+
+        self.send("""
+          <message to="user@example.com">
+            <x xmlns="jabber:x:conference"
+               jid="sleek@conference.jabber.org"
+               reason="Need to test Sleek" />
+          </message>
+        """)
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestStreamDirectInvite)

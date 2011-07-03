219df582dab2a5dd3c9e2bbfef27d3cfa814841d
Lance Stout <lancestout@gmail.com>
2011-07-02 22:49:34 -0700

    It isn't 2010 anymore.
    I keep forgetting to update the copyright on new code.

--- sleekxmpp/features/feature_bind/bind.py
+++ sleekxmpp/features/feature_bind/bind.py
@@ -1,6 +1,6 @@
 """
     SleekXMPP: The Sleek XMPP Library
-    Copyright (C) 2010  Nathanael C. Fritz
+    Copyright (C) 2011  Nathanael C. Fritz
     This file is part of SleekXMPP.
 
     See the file LICENSE for copying permission.

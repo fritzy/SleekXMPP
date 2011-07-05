b2d42b1d6cad5c7f77f41c813f1b8f421d9339af
Lance Stout <lancestout@gmail.com>
2011-07-05 11:18:46 -0700

    Test no longer applies to latest version of XEP-0066.

--- tests/test_stream_xep_0066.py
+++ tests/test_stream_xep_0066.py
@@ -40,33 +40,5 @@ class TestOOB(SleekTest):
 
         t.join()
 
-    def testReceiveOOB(self):
-        """Test receiving an OOB request."""
-        self.stream_start(plugins=['xep_0066', 'xep_0030'])
-
-        events = []
-
-        def receive_oob(iq):
-            events.append(iq['oob_transfer']['url'])
-
-        self.xmpp.add_event_handler('oob_transfer', receive_oob)
-
-        self.recv("""
-          <iq to="tester@localhost"
-              from="user@example.com"
-              type="set" id="1">
-            <query xmlns="jabber:iq:oob">
-              <url>http://github.com/fritzy/SleekXMPP/blob/master/README</url>
-              <desc>SleekXMPP README</desc>
-            </query>
-          </iq>
-        """)
-
-        time.sleep(0.1)
-
-        self.assertEqual(events,
-                ['http://github.com/fritzy/SleekXMPP/blob/master/README'],
-                'URL was not received: %s' % events)
-
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestOOB)

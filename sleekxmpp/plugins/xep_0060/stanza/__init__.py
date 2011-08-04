9ffdba8643ca8e52a30ce227331cd772a5fea6d1
Nathan Fritz <fritzy@netflint.net>
2011-08-03 23:56:24 -0700

    the great xep_0060 re-organization in preperation for rewrite

---
+++ sleekxmpp/plugins/xep_0060/stanza/__init__.py
@@ -0,0 +1,3 @@
+from pubsub import Pubsub, Affiliation, Affiliations, Subscription, Subscriptions, SubscribeOptions, Item, Items, Create, Publish, Retract, Unsubscribe, Subscribe, Configure, Options, PubsubState, PubsubStateEvent
+from pubsub_owner import PubsubOwner, DefaultConfig, OwnerAffiliations, OwnerAffiliation, OwnerConfigure, OwnerDefault, OwnerDelete, OwnerPurge, OwnerRedirect, OwnerSubscriptions, OwnerSubscription 
+from pubsub_event import Event, EventItem, EventRetract, EventItems, EventCollection, EventAssociate, EventDisassociate, EventConfiguration, EventPurge, EventSubscription 

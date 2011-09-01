24c5f8d374b78ff5efca967953155b46a38949e6
Lance Stout <lancestout@gmail.com>
2011-09-01 14:01:58 -0700

    Clean up pubsub#event stanzas.

--- sleekxmpp/plugins/xep_0060/stanza/pubsub_event.py
+++ sleekxmpp/plugins/xep_0060/stanza/pubsub_event.py
@@ -1,124 +1,112 @@
-from sleekxmpp.xmlstream.stanzabase import registerStanzaPlugin, ElementBase, ET, JID
-from sleekxmpp.stanza.iq import Iq
-from sleekxmpp.stanza.message import Message
-from sleekxmpp.basexmpp import basexmpp
-from sleekxmpp.xmlstream.xmlstream import XMLStream
-import logging
-from sleekxmpp.plugins import xep_0004
+"""
+    SleekXMPP: The Sleek XMPP Library
+    Copyright (C) 2011  Nathanael C. Fritz
+    This file is part of SleekXMPP.
+
+    See the file LICENSE for copying permission.
+"""
+
+from sleekxmpp import Message
+from sleekxmpp.xmlstream import register_stanza_plugin, ElementBase, ET, JID
+from sleekxmpp.plugins.xep_0004 import Form
+
 
 class Event(ElementBase):
-	namespace = 'http://jabber.org/protocol/pubsub#event'
-	name = 'event'
-	plugin_attrib = 'pubsub_event'
-	interfaces = set(('node',))
-	plugin_attrib_map = {}
-	plugin_tag_map = {}
+    namespace = 'http://jabber.org/protocol/pubsub#event'
+    name = 'event'
+    plugin_attrib = 'pubsub_event'
+    interfaces = set(('node',))
 
-registerStanzaPlugin(Message, Event)
 
 class EventItem(ElementBase):
-	namespace = 'http://jabber.org/protocol/pubsub#event'
-	name = 'item'
-	plugin_attrib = 'item'
-	interfaces = set(('id', 'payload'))
-	plugin_attrib_map = {}
-	plugin_tag_map = {}
+    namespace = 'http://jabber.org/protocol/pubsub#event'
+    name = 'item'
+    plugin_attrib = name
+    interfaces = set(('id', 'payload'))
 
-	def setPayload(self, value):
-		self.xml.append(value)
-	
-	def getPayload(self):
-		childs = self.xml.getchildren()
-		if len(childs) > 0:
-			return childs[0]
-	
-	def delPayload(self):
-		for child in self.xml.getchildren():
-			self.xml.remove(child)
+    def set_payload(self, value):
+        self.xml.append(value)
+
+    def get_payload(self):
+        childs = self.xml.getchildren()
+        if len(childs) > 0:
+            return childs[0]
+
+    def del_payload(self):
+        for child in self.xml.getchildren():
+            self.xml.remove(child)
 
 
 class EventRetract(ElementBase):
-	namespace = 'http://jabber.org/protocol/pubsub#event'
-	name = 'retract'
-	plugin_attrib = 'retract'
-	interfaces = set(('id',))
-	plugin_attrib_map = {}
-	plugin_tag_map = {}
+    namespace = 'http://jabber.org/protocol/pubsub#event'
+    name = 'retract'
+    plugin_attrib = name
+    interfaces = set(('id',))
+
 
 class EventItems(ElementBase):
-	namespace = 'http://jabber.org/protocol/pubsub#event'
-	name = 'items'
-	plugin_attrib = 'items'
-	interfaces = set(('node',))
-	plugin_attrib_map = {}
-	plugin_tag_map = {}
-	subitem = (EventItem, EventRetract)
+    namespace = 'http://jabber.org/protocol/pubsub#event'
+    name = 'items'
+    plugin_attrib = name
+    interfaces = set(('node',))
 
-registerStanzaPlugin(Event, EventItems)
 
 class EventCollection(ElementBase):
-	namespace = 'http://jabber.org/protocol/pubsub#event'
-	name = 'collection'
-	plugin_attrib = name
-	interfaces = set(('node',))
-	plugin_attrib_map = {}
-	plugin_tag_map = {}
+    namespace = 'http://jabber.org/protocol/pubsub#event'
+    name = 'collection'
+    plugin_attrib = name
+    interfaces = set(('node',))
 
-registerStanzaPlugin(Event, EventCollection)
 
 class EventAssociate(ElementBase):
-	namespace = 'http://jabber.org/protocol/pubsub#event'
-	name = 'associate'
-	plugin_attrib = name
-	interfaces = set(('node',))
-	plugin_attrib_map = {}
-	plugin_tag_map = {}
+    namespace = 'http://jabber.org/protocol/pubsub#event'
+    name = 'associate'
+    plugin_attrib = name
+    interfaces = set(('node',))
 
-registerStanzaPlugin(EventCollection, EventAssociate)
 
 class EventDisassociate(ElementBase):
-	namespace = 'http://jabber.org/protocol/pubsub#event'
-	name = 'disassociate'
-	plugin_attrib = name
-	interfaces = set(('node',))
-	plugin_attrib_map = {}
-	plugin_tag_map = {}
+    namespace = 'http://jabber.org/protocol/pubsub#event'
+    name = 'disassociate'
+    plugin_attrib = name
+    interfaces = set(('node',))
 
-registerStanzaPlugin(EventCollection, EventDisassociate)
 
 class EventConfiguration(ElementBase):
-	namespace = 'http://jabber.org/protocol/pubsub#event'
-	name = 'configuration'
-	plugin_attrib = name
-	interfaces = set(('node', 'config'))
-	plugin_attrib_map = {}
-	plugin_tag_map = {}
-	
-registerStanzaPlugin(Event, EventConfiguration)
-registerStanzaPlugin(EventConfiguration, xep_0004.Form)
+    namespace = 'http://jabber.org/protocol/pubsub#event'
+    name = 'configuration'
+    plugin_attrib = name
+    interfaces = set(('node', 'config'))
+
 
 class EventPurge(ElementBase):
-	namespace = 'http://jabber.org/protocol/pubsub#event'
-	name = 'purge'
-	plugin_attrib = name
-	interfaces = set(('node',))
-	plugin_attrib_map = {}
-	plugin_tag_map = {}
+    namespace = 'http://jabber.org/protocol/pubsub#event'
+    name = 'purge'
+    plugin_attrib = name
+    interfaces = set(('node',))
 
-registerStanzaPlugin(Event, EventPurge)
 
 class EventSubscription(ElementBase):
-	namespace = 'http://jabber.org/protocol/pubsub#event'
-	name = 'subscription'
-	plugin_attrib = name
-	interfaces = set(('node','expiry', 'jid', 'subid', 'subscription'))
-	plugin_attrib_map = {}
-	plugin_tag_map = {}
-	
-	def setJid(self, value):
-		self._setAttr('jid', str(value))
-	
-	def getJid(self):
-		return JID(self._getAttr('jid'))
+    namespace = 'http://jabber.org/protocol/pubsub#event'
+    name = 'subscription'
+    plugin_attrib = name
+    interfaces = set(('node', 'expiry', 'jid', 'subid', 'subscription'))
 
-registerStanzaPlugin(Event, EventSubscription)
+    def set_jid(self, value):
+        self._set_attr('jid', str(value))
+
+    def get_jid(self):
+        return JID(self._get_attr('jid'))
+
+
+register_stanza_plugin(Message, Event)
+register_stanza_plugin(Event, EventCollection)
+register_stanza_plugin(Event, EventConfiguration)
+register_stanza_plugin(Event, EventItems)
+register_stanza_plugin(Event, EventPurge)
+register_stanza_plugin(Event, EventSubscription)
+register_stanza_plugin(EventCollection, EventAssociate)
+register_stanza_plugin(EventCollection, EventDisassociate)
+register_stanza_plugin(EventConfiguration, Form)
+register_stanza_plugin(EventItems, EventItem, iterable=True)
+register_stanza_plugin(EventItems, EventRetract, iterable=True)

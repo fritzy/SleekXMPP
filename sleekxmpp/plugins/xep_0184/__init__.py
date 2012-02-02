2217c69757417ea83bb66ccb9aa13376d6324b4e
Erik Larsson <who+github@cnackers.org>
2012-02-02 14:29:27 +0100

    Added plugin for XEP-0184

---
+++ sleekxmpp/plugins/xep_0184/__init__.py
@@ -0,0 +1,10 @@
+"""
+    SleekXMPP: The Sleek XMPP Library
+    Copyright (C) 2012 Erik Reuterborg Larsson
+    This file is part of SleekXMPP.
+
+    See the file LICENSE for copying permission.
+"""
+
+from sleekxmpp.plugins.xep_0184.reciept import xep_0184
+from sleekxmpp.plugins.xep_0184.stanza import Request, Received

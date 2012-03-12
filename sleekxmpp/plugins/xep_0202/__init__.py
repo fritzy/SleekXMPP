6497857495d185401426ba31441e3039fb316891
Lance Stout <lancestout@gmail.com>
2012-03-12 00:22:42 -0700

    Move XEP-0202 to new system.

--- sleekxmpp/plugins/xep_0202/__init__.py
+++ sleekxmpp/plugins/xep_0202/__init__.py
@@ -6,7 +6,15 @@
     See the file LICENSE for copying permission.
 """
 
+from sleekxmpp.plugins.base import register_plugin
 
 from sleekxmpp.plugins.xep_0202 import stanza
 from sleekxmpp.plugins.xep_0202.stanza import EntityTime
-from sleekxmpp.plugins.xep_0202.time import xep_0202
+from sleekxmpp.plugins.xep_0202.time import XEP_0202
+
+
+register_plugin(XEP_0202)
+
+
+# Retain some backwards compatibility
+xep_0202 = XEP_0202

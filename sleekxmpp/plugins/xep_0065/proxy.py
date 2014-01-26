b95532b68b33787deef606e3ffc13e7416858b28
waechtjn <Jan_Waechter@gmx.de>
2014-01-26 16:48:31 +0100

    Update xep_0065/proxy.py
    Removed reference to undefined variable "conn"

--- sleekxmpp/plugins/xep_0065/proxy.py
+++ sleekxmpp/plugins/xep_0065/proxy.py
@@ -88,8 +88,9 @@ class XEP_0065(base_plugin):
 
         # Request that the proxy activate the session with the target.
         self.activate(proxy, sid, to, timeout=timeout)
-        self.xmpp.event('stream:%s:%s' % (sid, conn.peer_jid), conn)
-        return self.get_socket(sid)
+        socket = self.get_socket(sid)
+        self.xmpp.event('stream:%s:%s' % (sid, to), socket)
+        return socket
 
     def request_stream(self, to, sid=None, ifrom=None, block=True, timeout=None, callback=None):
         if sid is None:

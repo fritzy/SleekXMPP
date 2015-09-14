f2bf6072ec447cf5cc331b4057b29078b1c932b5
Robert Robinson <rerobins@gmail.com>
2015-09-14 16:35:47 -0600

    Add plugin (cherry picked from commit 2296d56)

--- sleekxmpp/plugins/xep_0122/stanza.py
+++ sleekxmpp/plugins/xep_0122/stanza.py
@@ -13,13 +13,17 @@ class FormValidation(ElementBase):
                 datatype='xs:dateTime'/>
       <value>2003-10-06T11:22:00-07:00</value>
     </field>
+
+    Questions:
+      Should this look at the datatype value and convert the range values as appropriate?
+      Should this stanza provide a pass/fail for a value from the field, or convert field value to datatype?
     """
 
     namespace = 'http://jabber.org/protocol/xdata-validate'
     name = 'validate'
     plugin_attrib = 'validate'
-    interfaces = set(('datatype', 'basic', 'open', 'range', 'regex', ))
-    sub_interfaces = set(('basic', 'open', 'range', 'regex', ))
+    interfaces = {'datatype', 'basic', 'open', 'range', 'regex', }
+    sub_interfaces = {'basic', 'open', 'range', 'regex', }
     plugin_attrib_map = {}
     plugin_tag_map = {}
 
@@ -33,22 +37,28 @@ class FormValidation(ElementBase):
         if value:
             self._add_field('basic')
         else:
-            self['basic'] = False
+            del self['basic']
 
     def set_open(self, value):
         if value:
             self._add_field('open')
+        else:
+            del self['open']
 
     def set_regex(self, regex):
         if regex:
             _regex = self._add_field('regex')
             _regex.text = regex
+        else:
+            del self['regex']
 
     def set_range(self, value, minimum=None, maximum=None):
         if value:
             _range = self._add_field('range')
             _range.attrib['min'] = str(minimum)
             _range.attrib['max'] = str(maximum)
+        else:
+            del self['range']
 
     def remove_all(self, except_tag=None):
         for a in self.sub_interfaces:
@@ -65,18 +75,20 @@ class FormValidation(ElementBase):
 
     def get_regex(self):
         present = self.xml.find('{%s}regex' % self.namespace)
-        if present:
+        if present is not None:
             return present.text
 
         return False
 
     def get_range(self):
-        present = self.xml.find('{%s}regex' % self.namespace)
-        if present:
-            return dict(present.attrib)
+        present = self.xml.find('{%s}range' % self.namespace)
+        if present is not None:
+            attributes = present.attrib
+            return_value = dict()
+            if 'min' in attributes:
+                return_value['minimum'] = attributes['min']
+            if 'max' in attributes:
+                return_value['maximum'] = attributes['max']
+            return return_value
 
         return False
-
-
-FormValidation.getBasic = FormValidation.get_basic
-FormValidation.setBasic = FormValidation.set_basic
--- tests/test_stanza_xep_0122.py
+++ tests/test_stanza_xep_0122.py
@@ -1,8 +1,7 @@
 import unittest
+
 from sleekxmpp import Message
 from sleekxmpp.test import SleekTest
-from sleekxmpp.thirdparty import OrderedDict
-
 import sleekxmpp.plugins.xep_0004 as xep_0004
 import sleekxmpp.plugins.xep_0122 as xep_0122
 from sleekxmpp.xmlstream import register_stanza_plugin
@@ -46,92 +45,113 @@ class TestDataForms(SleekTest):
           </message>
         """)
 
-    # def test_open_validation(self):
-    #     """Testing using multiple instructions elements in a data form."""
-    #     msg = self.Message()
-    #     form = msg['form']
-    #     field = form.addField(var='f1',
-    #                           ftype='text-single',
-    #                           label='Text',
-    #                           desc='A text field',
-    #                           required=True,
-    #                           value='Some text!')
-    #
-    #     validation = field['validate']
-    #     validation.set_open(True)
-    #
-    #     self.check(msg, """
-    #       <message>
-    #         <x xmlns="jabber:x:data" type="form">
-    #           <field var="f1" type="text-single" label="Text">
-    #             <desc>A text field</desc>
-    #             <required />
-    #             <value>Some text!</value>
-    #             <validate xmlns="http://jabber.org/protocol/xdata-validate">
-    #                 <open/>
-    #             </validate>
-    #           </field>
-    #         </x>
-    #       </message>
-    #     """)
-    #
-    # def test_regex_validation(self):
-    #     """Testing using multiple instructions elements in a data form."""
-    #     msg = self.Message()
-    #     form = msg['form']
-    #     field = form.addField(var='f1',
-    #                           ftype='text-single',
-    #                           label='Text',
-    #                           desc='A text field',
-    #                           required=True,
-    #                           value='Some text!')
-    #
-    #     validation = field['validate']
-    #     validation.set_regex('[0-9]+')
-    #
-    #     self.check(msg, """
-    #       <message>
-    #         <x xmlns="jabber:x:data" type="form">
-    #           <field var="f1" type="text-single" label="Text">
-    #             <desc>A text field</desc>
-    #             <required />
-    #             <value>Some text!</value>
-    #             <validate xmlns="http://jabber.org/protocol/xdata-validate">
-    #                 <regex>[0-9]+</regex>
-    #             </validate>
-    #           </field>
-    #         </x>
-    #       </message>
-    #     """)
-    #
-    # def test_range_validation(self):
-    #     """Testing using multiple instructions elements in a data form."""
-    #     msg = self.Message()
-    #     form = msg['form']
-    #     field = form.addField(var='f1',
-    #                           ftype='text-single',
-    #                           label='Text',
-    #                           desc='A text field',
-    #                           required=True,
-    #                           value='Some text!')
-    #
-    #     validation = field['validate']
-    #     validation.set_range(True, minimum=0, maximum=10)
-    #
-    #     self.check(msg, """
-    #       <message>
-    #         <x xmlns="jabber:x:data" type="form">
-    #           <field var="f1" type="text-single" label="Text">
-    #             <desc>A text field</desc>
-    #             <required />
-    #             <value>Some text!</value>
-    #             <validate xmlns="http://jabber.org/protocol/xdata-validate">
-    #                 <range min="0" max="10" />
-    #             </validate>
-    #           </field>
-    #         </x>
-    #       </message>
-    #     """)
+        self.assertTrue(validation.get_basic())
+        self.assertFalse(validation.get_open())
+        self.assertFalse(validation.get_range())
+        self.assertFalse(validation.get_regex())
+
+    def test_open_validation(self):
+        """Testing using multiple instructions elements in a data form."""
+        msg = self.Message()
+        form = msg['form']
+        field = form.addField(var='f1',
+                              ftype='text-single',
+                              label='Text',
+                              desc='A text field',
+                              required=True,
+                              value='Some text!')
+
+        validation = field['validate']
+        validation.set_open(True)
+
+        self.check(msg, """
+          <message>
+            <x xmlns="jabber:x:data" type="form">
+              <field var="f1" type="text-single" label="Text">
+                <desc>A text field</desc>
+                <required />
+                <value>Some text!</value>
+                <validate xmlns="http://jabber.org/protocol/xdata-validate">
+                    <open />
+                </validate>
+              </field>
+            </x>
+          </message>
+        """)
+
+        self.assertFalse(validation.get_basic())
+        self.assertTrue(validation.get_open())
+        self.assertFalse(validation.get_range())
+        self.assertFalse(validation.get_regex())
+
+    def test_regex_validation(self):
+        """Testing using multiple instructions elements in a data form."""
+        msg = self.Message()
+        form = msg['form']
+        field = form.addField(var='f1',
+                              ftype='text-single',
+                              label='Text',
+                              desc='A text field',
+                              required=True,
+                              value='Some text!')
+
+        regex_value = '[0-9]+'
+
+        validation = field['validate']
+        validation.set_regex(regex_value)
+
+        self.check(msg, """
+          <message>
+            <x xmlns="jabber:x:data" type="form">
+              <field var="f1" type="text-single" label="Text">
+                <desc>A text field</desc>
+                <required />
+                <value>Some text!</value>
+                <validate xmlns="http://jabber.org/protocol/xdata-validate">
+                    <regex>[0-9]+</regex>
+                </validate>
+              </field>
+            </x>
+          </message>
+        """)
+
+        self.assertFalse(validation.get_basic())
+        self.assertFalse(validation.get_open())
+        self.assertFalse(validation.get_range())
+        self.assertTrue(validation.get_regex())
+
+        self.assertEqual(regex_value, validation.get_regex())
+
+    def test_range_validation(self):
+        """Testing using multiple instructions elements in a data form."""
+        msg = self.Message()
+        form = msg['form']
+        field = form.addField(var='f1',
+                              ftype='text-single',
+                              label='Text',
+                              desc='A text field',
+                              required=True,
+                              value='Some text!')
+
+        validation = field['validate']
+        validation.set_range(True, minimum=0, maximum=10)
+
+        self.check(msg, """
+          <message>
+            <x xmlns="jabber:x:data" type="form">
+              <field var="f1" type="text-single" label="Text">
+                <desc>A text field</desc>
+                <required />
+                <value>Some text!</value>
+                <validate xmlns="http://jabber.org/protocol/xdata-validate">
+                    <range min="0" max="10" />
+                </validate>
+              </field>
+            </x>
+          </message>
+        """)
+
+        self.assertDictEqual(dict(minimum=str(0), maximum=str(10)), validation.get_range())
 
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestDataForms)
